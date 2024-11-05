4f763099bbd9173f01a22ef113aa06a3bc16e618
Christoph Pader <chris@margelo.com>
2024-11-05 10:55:36 +0100

    fix: lint and ts config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,17 @@ module.exports = {
   ],
   plugins: ['@typescript-eslint', 'prettier'],
   parser: '@typescript-eslint/parser',
+  ignorePatterns: [
+    "**/node_modules",
+    "**/lib",
+    "**/.eslintrc.js",
+    "**/.prettierrc.js",
+    "**/jest.config.js",
+    "**/babel.config.js",
+    "**/metro.config.js",
+    "**/react-native.config.js",
+    "**/tsconfig.json"
+  ],
   parserOptions: {
     project: true,
     tsconfigRootDir: __dirname,
--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   extends: '../.eslintrc.js',
   parserOptions: {
+    project: true,
     tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
   },
 };
